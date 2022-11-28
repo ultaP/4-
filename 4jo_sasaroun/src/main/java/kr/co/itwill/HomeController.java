3f7147fbf447ecc8953415683a4b6d5a61d30574
appleschatz <119024732+appleschatz@users.noreply.github.com>
2022-11-28 16:00:28 +0900

    Delete HomeController.java

--- 4jo_sasaroun/src/main/java/kr/co/itwill/HomeController.java
+++
@@ -1,8 +0,0 @@
-package kr.co.itwill;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class HomeController {
-}
